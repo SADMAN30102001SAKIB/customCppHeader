aa68995ceb00c5a1f8b8fde11f1314dbcb3c27af
SADMAN30102001SAKIB <SADMAN30102001SAKIB>
2023-03-07 21:46:27 +0600

    v1.0

---
+++ headerTest.cpp
@@ -0,0 +1,36 @@
+#include "sadman.h"
+moneiHobe namePoddhoti std thamo
+
+purnoShonkha tomraiKheloVai
+shuru
+
+    shobdo s1, s2 thamo
+    bairKoro eke "Duita Shobdo Dao: " thamo
+    dhukao oke s1 oke s2 thamo
+
+    jodi(s1 shoman s2)
+        shuru
+            bairKoro eke s1 eke " & " eke s2 eke " shoman" thamo
+        shesh    
+
+    naile
+        shuru
+            bairKoro eke s1 eke " & " eke s2 eke " shoman na" thamo
+        shesh
+
+    bairKoro eke notunLine thamo
+
+    vognangsho shonkha1 hocce 0.0 thamo
+    bairKoro eke "Ekta Shonkha Dao: " thamo
+    dhukao oke shonkha1 thamo
+    purnoShonkha shonkha2 hocce purnoShonkha(shonkha1) thamo
+
+    ghurao(i, 1, shonkha2)
+        shuru
+            bairKoro eke i eke " " thamo
+        shesh
+    
+
+    firieDao 0 thamo
+
+shesh
---
+++ sadman.h
@@ -0,0 +1,35 @@
+#ifndef SADMAN_H
+#define SADMAN_H
+
+#include<bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define purnoShonkha int
+#define vognangsho float
+#define borno char
+#define shobdo string
+#define tomraiKheloVai main()
+#define bairKoro cout
+#define dhukao cin
+#define eke <<
+#define oke >>
+#define thamo ;
+#define firieDao return
+#define shuru {
+#define shesh }
+#define jodi if
+#define abarJodi else if
+#define naile else
+#define hocce =
+#define shoman ==
+#define choto <
+#define boro >
+#define chotoShoman <=
+#define boroShoman >=
+#define notunLine endl
+#define moneiHobe using
+#define namePoddhoti namespace
+#define ghurao(a, b, c) for (int i = b; i <= c; i++)
+
+
+#endif
